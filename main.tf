528f3b0ee6d7b08bcee65c55429ca99d6fd7b21c
xat <descamps.jeremy@gmail.com>
2022-07-06 20:06:55 +0200

    Revert "test"
    This reverts commit 13329b20ae1c8d84b676ab884643cafa78780fd8.

--- main.tf
+++ main.tf
@@ -1,26 +1,17 @@
-locals {
-  test = "yr"
-  roles = [ for r in var.roles: { for p in r.principal_id: p => { name = try(r.name, null), scope = r.scope,definition_name = try(r.definition_name, null), definition_id = try(r.definition_id, null), description = try(r.description, null) }}]
-}
-
 resource "azurerm_user_assigned_identity" "this" {
-  for_each = var.identities
-
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  name = each.value.name
-  tags = each.value.tags
+  name = var.identity_name
+  tags = var.identity_tags
 }
 
 resource "azurerm_role_assignment" "this" {
-  for_each = local.roles
+  scope                = var.role_scope
+  role_definition_name = var.role_definition_name
+  role_definition_id   = var.role_definition_id
+  principal_id         = azurerm_user_assigned_identity.this.principal_id
+  description          = var.role_description
 
-  scope           = each.value.scope
-  definition_name = each.value.definition_name
-  definition_id   = each.value.definition_id
-  principal_id    = each.key
-  description     = each.value.description
-
-  name = each.value.name
+  name = var.role_name
 }
--- variables.tf
+++ variables.tf
@@ -12,71 +12,45 @@ variable "location" {
 
 # User identity variables
 ####
-variable "identities" {
-  description = "(Optional) A list of map that contains User Managed Identities."
-  type        = set(object({
-    name = string
-    tags = map(string)
-  }))
+variable "identity_name" {
+  description = "(Required) The name of the user assigned identity. Changing this forces a new identity to be created."
+  type        = string
 }
-# variable "identity_name" {
-#   description = "(Required) The name of the user assigned identity. Changing this forces a new identity to be created."
-#   type        = string
-# }
 
-# variable "identity_tags" {
-#   description = "A map of tags to add to user assigned identity resource."
-#   type        = map(string)
-#   default     = {}
-# }
+variable "identity_tags" {
+  description = "A map of tags to add to user assigned identity resource."
+  type        = map(string)
+  default     = {}
+}
 
 # Role assignment variables
 ####
 
-variable "roles" {
-  description = "(Optional) A list of map that contains roles assignments."
-  type        = list(object({
-    name            = string
-    scope           = string
-    definition_id   = string
-    principal_id    = list(string)
-    definition_name = string
-    description     = string
-  }))
-  default     = []
+variable "role_name" {
+  description = "(Optional) A unique UUID/GUID for this Role Assignment - one will be generated if not specified. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
 }
 
-# variable "role_name" {
-#   description = "(Optional) A unique UUID/GUID for this Role Assignment - one will be generated if not specified. Changing this forces a new resource to be created."
-#   type        = string
-#   default     = null
-# }
+variable "role_scope" {
+  description = "(Required) The scope at which the Role Assignment applies to, such as `/subscriptions/0b1f6471-1bf0-4dda-aec3-111122223333`, `/subscriptions/0b1f6471-1bf0-4dda-aec3-111122223333/resourceGroups/myGroup`, or `/subscriptions/0b1f6471-1bf0-4dda-aec3-111122223333/resourceGroups/myGroup/providers/Microsoft.Compute/virtualMachines/myVM`, or `/providers/Microsoft.Management/managementGroups/myMG`. Changing this forces a new resource to be created."
+  type        = string
+}
 
-# variable "role_scope" {
-#   description = "(Required) The scope at which the Role Assignment applies to, such as `/subscriptions/0b1f6471-1bf0-4dda-aec3-111122223333`, `/subscriptions/0b1f6471-1bf0-4dda-aec3-111122223333/resourceGroups/myGroup`, or `/subscriptions/0b1f6471-1bf0-4dda-aec3-111122223333/resourceGroups/myGroup/providers/Microsoft.Compute/virtualMachines/myVM`, or `/providers/Microsoft.Management/managementGroups/myMG`. Changing this forces a new resource to be created."
-#   type        = string
-# }
+variable "role_definition_id" {
+  description = "(Optional) The Scoped-ID of the Role Definition. Changing this forces a new resource to be created. Conflicts with `role_definition_name`."
+  type        = string
+  default     = null
+}
 
-# variable "role_definition_id" {
-#   description = "(Optional) The Scoped-ID of the Role Definition. Changing this forces a new resource to be created. Conflicts with `role_definition_name`."
-#   type        = string
-#   default     = null
-# }
+variable "role_definition_name" {
+  description = "(Optional) The name of a built-in Role. Changing this forces a new resource to be created. Conflicts with `role_definition_id`."
+  type        = string
+  default     = null
+}
 
-# variable "role_principal_id" {
-#   description = "(Required) The ID of the Principal (User, Group or Service Principal) to assign the Role Definition to. Changing this forces a new resource to be created."
-#   type        = string
-#   default     = "48031b6e-1bad-4ba0-bb2d-955609fcaf2e"
-# }
-
-# variable "role_definition_name" {
-#   description = "(Optional) The name of a built-in Role. Changing this forces a new resource to be created. Conflicts with `role_definition_id`."
-#   type        = string
-#   default     = null
-# }
-
-# variable "role_description" {
-#   description = "(Optional) The description for this Role Assignment. Changing this forces a new resource to be created."
-#   type        = string
-#   default     = null
-# }
+variable "role_description" {
+  description = "(Optional) The description for this Role Assignment. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
